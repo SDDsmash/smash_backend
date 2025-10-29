bd6a7ca0e67baa26453ea47405d44903f7fa9b87
LeeHyunWoo02 <hyoun0824@naver.com>
2025-10-29 19:10:47 +0900

    feature : 법정동 코드 패치 로직 추가

---
+++ src/main/java/SDD/smash/Address/Converter/AddressConverter.java
@@ -0,0 +1,26 @@
+package SDD.smash.Address.Converter;
+
+import SDD.smash.Address.Dto.SidoDTO;
+import SDD.smash.Address.Dto.SigunguDTO;
+import SDD.smash.Address.Entity.Sido;
+import SDD.smash.Address.Entity.Sigungu;
+
+import static SDD.smash.Util.BatchUtil.clean;
+
+public class AddressConverter {
+    public static Sido toEntity(SidoDTO dto){
+        return Sido.builder()
+                .name(dto.getName())
+                .sidoCode(clean(dto.getSido_code()))
+                .build();
+    }
+
+    public static Sigungu toEntity(SigunguDTO dto, Sido sido){
+        return Sigungu.builder()
+                .name(dto.getName())
+                .sigunguCode(dto.getSigungu_code())
+                .sido(sido)
+                .build();
+
+    }
+}
