aad023e8c14ed428dce7663c7fd0cfa20a319af5
LeeHyunWoo02 <hyoun0824@naver.com>
2025-10-29 20:00:32 +0900

    refeact : 메소드 명 수정

--- src/main/java/SDD/smash/Address/Converter/AddressConverter.java
+++ src/main/java/SDD/smash/Address/Converter/AddressConverter.java
@@ -8,14 +8,14 @@ import SDD.smash.Address.Entity.Sigungu;
 import static SDD.smash.Util.BatchUtil.clean;
 
 public class AddressConverter {
-    public static Sido toEntity(SidoDTO dto){
+    public static Sido sidoToEntity(SidoDTO dto){
         return Sido.builder()
                 .name(dto.getName())
                 .sidoCode(clean(dto.getSido_code()))
                 .build();
     }
 
-    public static Sigungu toEntity(SigunguDTO dto, Sido sido){
+    public static Sigungu sigunguToEntity(SigunguDTO dto, Sido sido){
         return Sigungu.builder()
                 .name(dto.getName())
                 .sigunguCode(dto.getSigungu_code())
