f41dfa15cb87c24db51e90877c4b8322820bc007
신진범 <140528690+bumsoft@users.noreply.github.com>
2025-10-31 19:24:53 +0900

    feat : 캐시용 DTO 구현

---
+++ src/main/java/SDD/smash/Support/dto/SupportDTO.java
@@ -0,0 +1,17 @@
+package SDD.smash.Support.dto;
+
+import SDD.smash.Support.domain.SupportTag;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+public class SupportDTO {
+    String plcyNm;
+    String aplyUrlAddr;
+    String plcyKywdNm;
+}
