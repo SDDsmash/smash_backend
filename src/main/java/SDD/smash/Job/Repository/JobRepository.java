b7287399e126b587f0bf94c09c2e59aca48c310a
신진범 <140528690+bumsoft@users.noreply.github.com>
2025-11-01 15:46:56 +0900

    feat : 일자리 관련 리포

--- src/main/java/SDD/smash/Job/Repository/JobRepository.java
+++ src/main/java/SDD/smash/Job/Repository/JobRepository.java
@@ -1,7 +1,56 @@
 package SDD.smash.Job.Repository;
 
+import SDD.smash.Job.Dto.JobCountDTO;
+import SDD.smash.Job.Dto.JobInfoDTO;
 import SDD.smash.Job.Entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
+
+    @Query("""
+        SELECT new SDD.smash.Job.Dto.JobCountDTO(
+        j.sigungu.sigunguCode,
+        SUM(j.count)
+        )
+        FROM Job j
+        GROUP BY j.sigungu.sigunguCode
+    """)
+    List<JobCountDTO> findAllTotalJobCount();
+
+
+    @Query("""
+        SELECT new SDD.smash.Job.Dto.JobCountDTO(
+        j.sigungu.sigunguCode,
+        j.count
+        )
+        FROM Job j
+        WHERE j.jobCodeMiddle.code = :middleCode
+    """)
+    List<JobCountDTO> findAllJobCode(@Param("middleCode") String middleCode);
+
+
+    @Query("""
+        SELECT new SDD.smash.Job.Dto.JobInfoDTO(
+        SUM(j.count)
+        )
+        FROM Job j
+        WHERE j.sigungu.sigunguCode = :sigunguCode
+    """)
+    JobInfoDTO findJobInfo(@Param("sigunguCode") String sigunguCode);
+
+    @Query("""
+        SELECT new SDD.smash.Job.Dto.JobInfoDTO(
+        j.count
+        )
+        FROM Job j
+        WHERE j.sigungu.sigunguCode = :sigunguCode
+        AND j.jobCodeMiddle.code = :middleCode
+    """)
+    JobInfoDTO findJobInfoByCode(@Param("sigunguCode") String sigunguCode, @Param("middleCode") String middleCode);
+
+
 }
