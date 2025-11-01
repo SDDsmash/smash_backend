85e93af1841115b9b498cb066537501cd5945011
신진범 <140528690+bumsoft@users.noreply.github.com>
2025-11-01 15:46:34 +0900

    feat : 지역의 일자리 정보 반환 서비스

---
+++ src/main/java/SDD/smash/Job/Dto/JobInfoDTO.java
@@ -0,0 +1,22 @@
+package SDD.smash.Job.Dto;
+
+import lombok.Setter;
+
+public class JobInfoDTO {
+
+    private Long count;
+
+    @Setter
+    private String url;
+
+    public JobInfoDTO(Long count)
+    {
+        this.count = (count == null) ? 0L : count;
+    }
+
+    public JobInfoDTO(Integer count)
+    {
+        this.count = (count == null) ? 0L : count.longValue();
+    }
+
+}
