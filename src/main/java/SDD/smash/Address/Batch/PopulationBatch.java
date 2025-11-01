4c2c33f5f40ccb5732eee346b8f3fcb3a01e258d
LeeHyunWoo02 <hyoun0824@naver.com>
2025-11-01 19:32:04 +0900

    refeact : 인구수 JPA -> JDBC SQL

--- src/main/java/SDD/smash/Address/Batch/PopulationBatch.java
+++ src/main/java/SDD/smash/Address/Batch/PopulationBatch.java
@@ -1,11 +1,10 @@
 package SDD.smash.Address.Batch;
 
 import SDD.smash.Address.Dto.PopulationDTO;
-import SDD.smash.Address.Entity.Population;
+import SDD.smash.Address.Dto.PopulationUpsertDTO;
 import SDD.smash.Address.Entity.Sigungu;
-import SDD.smash.Address.Repository.PopulationRepository;
 import SDD.smash.Address.Repository.SigunguRepository;
-import SDD.smash.Util.BatchTextUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -14,54 +13,45 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.data.RepositoryItemWriter;
-import org.springframework.batch.item.data.builder.RepositoryItemWriterBuilder;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.sql.DataSource;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-import static SDD.smash.Address.Converter.AddressConverter.*;
 import static SDD.smash.Util.BatchTextUtil.*;
 
 
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class PopulationBatch {
     private final JobRepository jobRepository;
     private final PlatformTransactionManager platformTransactionManager;
-    private final PopulationRepository populationRepository;
     private final SigunguRepository sigunguRepository;
+    private final @Qualifier("dataDBSource") DataSource dataDataSource;
 
-    public PopulationBatch(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager, PopulationRepository populationRepository, SigunguRepository sigunguRepository) {
-        this.jobRepository = jobRepository;
-        this.platformTransactionManager = platformTransactionManager;
-        this.populationRepository = populationRepository;
-        this.sigunguRepository = sigunguRepository;
-    }
 
-    private Map<String, Sigungu> sigunguCache  = null;
+    private Set<String> sigunguCodeCache = null;
 
-    private Sigungu resolveSigungu(String sigunguCode) {
-        if (sigunguCache == null) {
-            sigunguCache = new HashMap<>();
-            for (Sigungu sigungu : sigunguRepository.findAll()) {
-                sigunguCache.put(sigungu.getSigunguCode(), sigungu);
-            }
+    private boolean isKnownSigunguCode(String code) {
+        if (sigunguCodeCache == null) {
+            sigunguCodeCache = sigunguRepository.findAll()
+                    .stream()
+                    .map(Sigungu::getSigunguCode)
+                    .collect(Collectors.toSet());
         }
-        Sigungu sigungu = sigunguCache.get(sigunguCode);
-        if (sigungu == null) {
-            log.warn("❗ Unknown sigungu code: {}", sigunguCode);
-            return null; // 혹은 예외 throw
-        }
-        String sigunguCodeTest = sigungu.getSigunguCode();
-        return sigunguCache.get(sigunguCode);
+        return sigunguCodeCache.contains(code);
     }
 
     @Value("${population.filePath}")
@@ -78,7 +68,7 @@ public class PopulationBatch {
     public Step populationStep() {
 
         return new StepBuilder("populationStep", jobRepository)
-                .<PopulationDTO, Population> chunk(100, platformTransactionManager)
+                .<PopulationDTO, PopulationUpsertDTO> chunk(100, platformTransactionManager)
                 .reader(populationCsvReader())
                 .processor(populationCsvProcessor())
                 .writer(populationWriter())
@@ -109,24 +99,29 @@ public class PopulationBatch {
     }
 
     @Bean
-    public ItemProcessor<PopulationDTO, Population> populationCsvProcessor(){
+    public ItemProcessor<PopulationDTO, PopulationUpsertDTO> populationCsvProcessor(){
         return dto -> {
-            String sigunguKey = dto.getSigungu_code();
-            if (BatchTextUtil.isBlank(sigunguKey)) {
-                log.warn("❗ Empty sigungu key. Skip row.");
-                return null; // 스킵
-            }
-            Sigungu sigungu = resolveSigungu(sigunguKey);
-
-            return populationToEntity(dto, sigungu);
+            String sigunguCode = dto.getSigungu_code();
+            if (isBlank(sigunguCode) || !isKnownSigunguCode(sigunguCode)) return null;
+            return PopulationUpsertDTO.builder()
+                    .sigunguCode(sigunguCode)
+                    .population(dto.getPopulation())
+                    .build();
         };
     }
     @Bean
-    public RepositoryItemWriter<Population> populationWriter() {
+    public JdbcBatchItemWriter<PopulationUpsertDTO> populationWriter() {
+        String upsertSql = """
+        INSERT INTO Population (sigungu_code, population_count)
+        VALUES (:sigunguCode, :population)
+        ON DUPLICATE KEY UPDATE population_count = VALUES(population_count)
+            """;
 
-        return new RepositoryItemWriterBuilder<Population>()
-                .repository(populationRepository)
-                .methodName("save")
+        return new JdbcBatchItemWriterBuilder<PopulationUpsertDTO>()
+                .dataSource(dataDataSource)
+                .sql(upsertSql)
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .assertUpdates(false)
                 .build();
     }
 
