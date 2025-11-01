84a7c12fe4a94cb6448c089220a8f022a761616d
신진범 <140528690+bumsoft@users.noreply.github.com>
2025-11-01 15:45:56 +0900

    feat : 직종코드에 대한 에러코드 추가

--- src/main/java/SDD/smash/Job/Repository/JobCodeMiddleRepository.java
+++ src/main/java/SDD/smash/Job/Repository/JobCodeMiddleRepository.java
@@ -4,4 +4,6 @@ import SDD.smash.Job.Entity.JobCodeMiddle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JobCodeMiddleRepository extends JpaRepository<JobCodeMiddle, String> {
+
+    boolean existsByCode(String code);
 }
