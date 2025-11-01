0a63dd7b5e17ac5930eb42ce5f913a486536bf19
신진범 <140528690+bumsoft@users.noreply.github.com>
2025-11-02 00:32:44 +0900

    feat : 개발환경 도커로 통일

--- src/main/java/SDD/smash/Config/PropertyConfig.java
+++
@@ -1,9 +0,0 @@
-package SDD.smash.Config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:env.properties")
-public class PropertyConfig {
-}
