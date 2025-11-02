8b6e989526ad586cbe909c17314746787d9d4e87
LeeHyunWoo02 <hyoun0824@naver.com>
2025-11-03 01:30:01 +0900

    refeact : DwellingScore 레포지토리 삭제

--- src/main/java/SDD/smash/Dwelling/Repository/DwellingScoreRepository.java
+++
@@ -1,6 +0,0 @@
-package SDD.smash.Dwelling.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DwellingScoreRepository extends JpaRepository<DwellingScore,String> {
-}
